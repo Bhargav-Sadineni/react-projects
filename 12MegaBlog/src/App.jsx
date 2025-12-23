bf66229f6f8dd970262a60e3211b031f6b3c972d
Bhargav-Sadineni <sadinenibhargav0@gmail.com>
2025-12-23 14:07:06 +0530

    todo, local storage and context api

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App"
+
+function App() {
+  console.log(import.meta.env.VITE_NUMBER)
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
